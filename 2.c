4e541677486104bf4ff3baf7d54ae72863f7a99e
sahil <lathiyasahil00@gmail.com>
2023-08-17 18:27:47 +0530

    ds

---
+++ 2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+    int a=15;
+    // printf("A: %d , A: %d , A: %d , A: %d , A: %d", a, a++, a++,++a,++a ); //           
+    // printf("A: %d , A: %d , A: %d , A: %d , A: %d", a, a--, --a,++a,a-- ); //           
+    // printf("A: %d , A: %d , A: %d , A: %d , A: %d", a, a++, a++,++a,++a ); //           
+
+}
