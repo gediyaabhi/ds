d170e3c10d0831677d210380677088e87d7d6524
Dhaval Solanki <dhavalas24@gmail.com>
2023-08-18 18:14:35 +0530

    ds

---
+++ Untitled-1.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main ()
+{
+    int c,a,b,nn,n,i;
+
+    printf("enter a value :");
+    scanf("%d",&n);
+
+    // printf("enter a value :");
+    // scanf("%d",&nn);
+
+    for(i=0;i<n;i++)
+    {
+        printf("first value in array :");
+        scanf("%d",&n);
+        // c=a[i];
+    }
+
+    // for(i=0;i<nn;i++)
+    // {
+    //     printf("second value in array :");
+    //     scanf("%d",&nn);
+    //     //  c=a[i];
+    // }
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d",n);
+    }
+
+    //  for(i=0;i<nn;i++)
+    // {
+    //     printf("%d",nn);
+    // }
+
+
+    // for(i=0;i<n;i++)
+    // {
+    //     // c=a[i];
+
+    //     printf("%d",c);
+    // }
+
+    // for(i=0;i<nn;i++)
+    // {
+    //     // c=b[i];
+    //     // c=b[i];
+    //     printf("%d",c);
+    // }
+}
