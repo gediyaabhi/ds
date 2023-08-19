50985180e769f673c01597493fad62b98f0dae19
Dhaval Solanki <dhavalas24@gmail.com>
2023-08-19 17:16:09 +0530

    ds

---
+++ array333.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int i,n,b;
+
+    printf("enter a first value :");
+    scanf("%d",&n);
+
+    int a[n];
+
+    for(i=0;i<n;i++)
+    {
+        printf("enter a first value :");
+        scanf("%d",&a[i]);
+    }
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,51 @@
+
+// insert first  values && first delete
+#include<stdio.h>
+#define n 5
+int a[n],top =-1;
+int insertend(int val)
+{
+    if (top>=n-1)
+        printf("Array is full....\n");
+    else 
+    {
+        top++;
+        a[top]=val;
+    }
+}
+
+int display ()
+{
+    if (top<0)
+        printf("Array is empty....\n");
+    else
+    {
+        for(int i=0; i<=top;i++)
+            printf("%d",a[i]);
+    }
+}
+
+int dlt()
+{
+    if (top<0)
+        printf("Array is empty....\n");
+    else
+    {
+       top--;
+    }
+}
+
+int main ()
+{
+    insertend(10);
+    insertend(20);
+    insertend(30);
+    insertend(40);
+    insertend(50);
+    insertend(60);
+    insertend(70);
+
+    dlt();
+    dlt();
+    display();
+}
