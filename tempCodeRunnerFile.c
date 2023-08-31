8282f9050f74376552b0f925b9fcf30103bc8030
Dhaval Solanki <dhavalas24@gmail.com>
2023-08-31 18:04:01 +0530

    ds

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,41 @@
 #include<stdio.h>
-void main ()
+struct node {
+    int data;
+    char ch;
+    struct node *ptr;
+};
+
+int main()
 {
-    printf("hello world");
+    struct node a,b,c,d;
+    a.ch = 'A';
+    a.data= 50;
+    a.ptr= NULL;
+
+
+    b.ch='b';
+    b.data= 100;
+    b.ptr= NULL;
+
+    c.ch='c';
+    c.data=150;
+    c.ptr=NULL;
+
+    d.ch='d';
+    d.data=200;
+    d.ptr=NULL;
+
+    printf(" A Data : %d \tchar : %c ", a.data,a.ch);
+    printf("\n B Data : %d \tchar : %c ", b.data,b.ch);
+    printf("\n C Data : %d \tchar : %c ", c.data,c.ch);
+    printf("\n D Data : %d \tchar : %c ", d.data,d.ch);
+
+    a.ptr=&b;
+    b.ptr=&a;
+    c.ptr=&d;
+    d.ptr=&c;
+    printf("\n\n\n B Data : %d \tchar : %c ",a.ptr->data, a.ptr->ch);
+    printf("\n A Data : %d \t char : %c ", b.ptr->data,b.ptr->ch);
+    printf("\n C data : %d \t char : %c", d.ptr->data,d.ptr->ch);
+    printf("\n D data : %d \tchar : %c",c.ptr->data,c.ptr->ch);
 }
